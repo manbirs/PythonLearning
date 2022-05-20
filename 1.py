8b3104d9a9f66d43779cb41d0420513b83ccabc5
Manbir S <32782324+manbirs@users.noreply.github.com>
2022-05-20 07:14:18 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+"""This is a simple user input project. User provides an input and code is executed with concatination of pre-defined text and user input"""
+
+#User enter company name
+Company_Name = input("What is your company name?")
+#User enter company address
+Company_Address  = input ("What is your company address?")
+#Code will return the concatination of pre-defined text and user input
+print ("I work for"+" " + Company_Name + " and it is located at" + " " + Company_Address)
+
